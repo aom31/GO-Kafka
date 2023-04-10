a78fc618ad85114adcd23f71cb5aef263536399a
thamakorn.k <thamakorn.aom31@gmail.com>
2023-04-10 23:37:51 +0700

    feat(handler-consumer) : add handler consumer event topic kafka

---
+++ consumer-service/services/account-handler.go
@@ -0,0 +1,53 @@
+package services
+
+import (
+	"encoding/json"
+	"events"
+	"log"
+	"reflect"
+
+	"consumer-service/repositories"
+)
+
+type EventHandler interface {
+	Handle(topic string, eventBytes []byte)
+}
+
+type accountEventHandler struct {
+	accountRepo repositories.AccountRepository
+}
+
+func NewAccountEventHandler(accountRepo repositories.AccountRepository) EventHandler {
+	return accountEventHandler(accountRepo)
+}
+
+func (obj accountEventHandler) Handle(topic string, eventBytes []byte) {
+	switch topic {
+	case reflect.TypeOf(events.OpenAccountEvent{}).Name():
+		event := &events.OpenAccountEvent{}
+		err := json.Unmarshal(eventBytes, event)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		bankAccount := repositories.BankAccount{
+			ID:            event.ID,
+			AccountHolder: event.AccountHolder,
+			AccountType:   event.AccountType,
+			Balance:       event.OpeningBalance,
+		}
+		err = obj.accountRepo.Save(bankAccount)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	case reflect.TypeOf(events.DepositFundEvent{}).Name():
+		event := &events.DepositFundEvent{}
+		err := json.Unmarshal(eventBytes, event)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
+}
