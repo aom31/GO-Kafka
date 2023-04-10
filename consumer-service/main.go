3656ef583d0062b436b20819cdf9654d21fde0b3
thamakorn.k <thamakorn.aom31@gmail.com>
2023-04-10 21:32:02 +0700

    feat: init consumer service

---
+++ consumer-service/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/Shopify/sarama"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+}
+func main() {
+
+	consumerGroup, err := sarama.NewConsumerGroup(viper.GetStringSlice("kafka.servers"), viper.GetString("kafka.group"), nil)
+	if err != nil {
+		panic(err)
+	}
+	defer consumerGroup.Close()
+
+}
